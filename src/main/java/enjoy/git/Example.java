77b0c6249ad67301eba8191157c77502587dda57
pengjing <pengjing6@outlook.com>
2019-09-27 13:09:38 +0800

    new git

---
+++ src/main/java/enjoy/git/Example.java
@@ -0,0 +1,22 @@
+package enjoy.git;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    public String home(){
+        return "hello git";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class,args);
+    }
+}
