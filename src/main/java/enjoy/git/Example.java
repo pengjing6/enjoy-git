1023e62aee68c5fc244e66ead181c9b2e0a34305
pengjing <pengjing6@outlook.com>
2019-09-27 19:42:00 +0800

    pull

--- src/main/java/enjoy/git/Example.java
+++ src/main/java/enjoy/git/Example.java
@@ -3,7 +3,6 @@ package enjoy.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +20,5 @@ public class Example {
     public static void main(String[] args) {
         SpringApplication.run(Example.class,args);
     }
+
 }
